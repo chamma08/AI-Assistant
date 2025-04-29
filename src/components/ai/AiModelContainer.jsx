f6ea60a3b98d2649d8e45d48422c046ee1948c1e
Matheesha Dissanayake <SA22524904@my.sliit.lk>
2025-04-29 09:37:58 +0530

    updated

--- src/components/ai/AiModelContainer.jsx
+++ src/components/ai/AiModelContainer.jsx
@@ -1,7 +1,7 @@
 import { OrbitControls, PerspectiveCamera, Stage } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
-import { A } from "./A";
+import { E } from "./E";
 
 const AiModelContainer = () => {
   return (
@@ -12,11 +12,11 @@ const AiModelContainer = () => {
       }}
       >
       <Suspense fallback="loading...">
-        <Stage environment="night" intensity={5}>
-          <A position={[0,70,0]}/>
+        <Stage environment="sunset" intensity={5}>
+          <E position={[0,10,0]}/>
         </Stage>
         <OrbitControls enableZoom={false}/>
-        <PerspectiveCamera position={[0, -40, 200]} zoom={2.7} makeDefault />
+        <PerspectiveCamera position={[0, -40, 200]} zoom={1.1} makeDefault />
       </Suspense>
     </Canvas>
   );
--- src/components/services/computer/ComputerModelContainer.jsx
+++ src/components/services/computer/ComputerModelContainer.jsx
@@ -1,17 +1,17 @@
 import { Canvas } from "@react-three/fiber";
 import { Suspense } from "react";
-import { A } from "./A";
+import { E } from "./E";
 import { OrbitControls, PerspectiveCamera, Stage } from "@react-three/drei";
 
 const ComputerModelContainer = () => {
   return (
     <Canvas>
       <Suspense fallback="loading...">
-        <Stage environment="night" intensity={5}>
-          <A />
+        <Stage environment="sunset" intensity={5}>
+          <E />
         </Stage>
         <OrbitControls enableZoom={false} autoRotate/>
-        <PerspectiveCamera position={[0,0,200]} zoom={0.8} makeDefault/>
+        <PerspectiveCamera position={[0,0,200]} zoom={1.1} makeDefault/>
       </Suspense>
     </Canvas>
   );
